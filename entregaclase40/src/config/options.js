c6fda850e6d5c42931e69dbd17e9d1ed87f4eb34
efontecha <kitservi@hotmail.com>
2023-03-09 21:42:20 -0500

    Entrega desafio clase40

---
+++ entregaclase40/src/config/options.js
@@ -0,0 +1,15 @@
+//todas las Variables que mi proyecto usa par funcionar
+// luego en otra clase se conecta con variables globales
+// antes npm i  mongoose   
+export const options = {
+    FileSystem: {},
+    firebase: {},
+    aws: {},
+    mongo: {
+        url: 'mongodb+srv://kitservi:React123@codermongoatlas.n8dxdd1.mongodb.net/capasDB?retryWrites=true&w=majority'
+    }
+}
+
+//credenciales mongoose atlas
+// usuario : kitservi
+//contraseña: React123
