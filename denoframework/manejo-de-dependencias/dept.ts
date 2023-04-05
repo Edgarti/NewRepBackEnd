3e59358ad62fb09fe19190a985bdbdb567aaf8c4
efontecha <kitservi@hotmail.com>
2023-04-04 20:13:09 -0500

    Entrega desafio  Deno framework oak

---
+++ denoframework/manejo-de-dependencias/dept.ts
@@ -0,0 +1,4 @@
+export { config } from "https://deno.land/x/dotenv@v3.2.2/mod.ts";
+export { format } from "https://deno.land/std@0.125.0/datetime/mod.ts";
+export { serve } from "https://deno.land/std@0.178.0/http/server.ts";
+export { bgBlue } from "https://deno.land/std@0.182.0/fmt/colors.ts";
